c6ee628c0e9e7956f27c749bef7026c60b374dda
Manisha kumre <manisha@deltafour.co>
2025-12-23 18:49:09 +0530

    Initial Commit

---
+++ Lloyds/LloydsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LloydsApp.swift
+//  Lloyds
+//
+//  Created by Manisha on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LloydsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
